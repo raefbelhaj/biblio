8b8d2b09ace4ee7ccaa4b6128feef72ba2c07efe
jasser98 <jasserjammeli98@gmail.com>
2023-11-25 22:21:30 +0100

    1er

---
+++ src/main/java/tech/getaarays/BibliothequeExpress/BibliothequeExpressApplication.java
@@ -0,0 +1,13 @@
+package tech.getaarays.BibliothequeExpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliothequeExpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliothequeExpressApplication.class, args);
+	}
+
+}
